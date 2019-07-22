8bc4a7cc83f4aafcaddf2199260f013a7d40d4cd
Vika <victoriaspiridonova95@gmail.com>
2019-07-22 21:23:27 +0700

    -content

--- src/components/content/content.component.js
+++ src/components/content/content.component.js
@@ -15,19 +15,28 @@ export class Content extends React.Component {
                             <p className="author-content"><span className="text-secondary">written by</span> Victoria
                             </p>
                             <div className="line-content mx-auto mb-3"></div>
-                            <p className="text-content text-left font-weight-light">Lommodo ligula eget dolor. Aenean massa. Cum sociis que
-                                penatibus et magnis dis parturient montes lorem, nascetur ridiculus mus. Donec quam
-                                felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla onsequat massa quis
-                                enim. Donec pede justo fringilla vel aliquet nec vulputate eget. Lorem ispum dolore
-                                siamet ipsum dolor.
-
-                                Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta
-                                nobis est eligendi optio cumquer nihil impedit quo minus id quod maxime placeat facere.
-                                At vero eos et accusamus et iusto odio dignissimos ducimus quilor.</p>
-                        <button type="button" className="btn btn-content btn-success text-white mb-3">CONTINUE READING</button>
                         </div>
-                        <div className="data-box">
-                                    j
+                        <p className="text-content text-justify font-weight-light">Lommodo ligula eget dolor. Aenean
+                            massa. Cum sociis que
+                            penatibus et magnis dis parturient montes lorem, nascetur ridiculus mus. Donec quam
+                            felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla onsequat massa quis
+                            enim. Donec pede justo fringilla vel aliquet nec vulputate eget. Lorem ispum dolore
+                            siamet ipsum dolor.
+
+                            Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta
+                            nobis est eligendi optio cumquer nihil impedit quo minus id quod maxime placeat facere.
+                            At vero eos et accusamus et iusto odio dignissimos ducimus quilor.</p>
+                        <button type="button" className="btn btn-content btn-success text-white mb-4">CONTINUE READING
+                        </button>
+                        <div className="data-box text-left text-secondary mb-5">
+                            <p>
+                                <i className="material-icons mr-1">
+                                    query_builder
+                                </i>
+                                July 22, 2019<span className="mx-2">|</span>
+                                <i className="material-icons ml-2 mr-1">
+                                chat_bubble_outline
+                            </i> 0 comment</p>
                         </div>
                     </div>
                     <div className="col-sm-4">
