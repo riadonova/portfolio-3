3ad7bf8c827259ca517be7109c74d58948d10a3c
Vika <victoriaspiridonova95@gmail.com>
2019-10-07 14:46:18 +0700

    publish

---
+++ docs/precache-manifest.930d6df8e14e78aaf29e0e102a640956.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eaf79b34ed8b9e3b34b2ebdaecc9436a",
+    "url": "/portfolio-3/index.html"
+  },
+  {
+    "revision": "27f53d4171a4b06143df",
+    "url": "/portfolio-3/static/css/2.34c9b3d3.chunk.css"
+  },
+  {
+    "revision": "c659539499854f47ee53",
+    "url": "/portfolio-3/static/css/main.7a861ac4.chunk.css"
+  },
+  {
+    "revision": "27f53d4171a4b06143df",
+    "url": "/portfolio-3/static/js/2.919797da.chunk.js"
+  },
+  {
+    "revision": "c659539499854f47ee53",
+    "url": "/portfolio-3/static/js/main.8f3d8476.chunk.js"
+  },
+  {
+    "revision": "146ec39749a1ef49442c",
+    "url": "/portfolio-3/static/js/runtime~main.3dbb8ade.js"
+  },
+  {
+    "revision": "4cafb54917a83db6a6501e88ca917405",
+    "url": "/portfolio-3/static/media/carousel-img-1.4cafb549.jpg"
+  },
+  {
+    "revision": "53d10cffacc58468ad35b517ed645bab",
+    "url": "/portfolio-3/static/media/carousel-img-2.53d10cff.jpg"
+  },
+  {
+    "revision": "06dc2a3967c0c0ac2f5a5d9447cfdcdf",
+    "url": "/portfolio-3/static/media/carousel-img-3.06dc2a39.jpg"
+  },
+  {
+    "revision": "1448ea07da6a97f4be9305fba76af045",
+    "url": "/portfolio-3/static/media/carousel-img-4.1448ea07.jpg"
+  },
+  {
+    "revision": "13bea0233183cd6350bd7328d5096d15",
+    "url": "/portfolio-3/static/media/carousel-img-5.13bea023.jpg"
+  },
+  {
+    "revision": "72bf7ddd9f608175af3420ead6f8d2e9",
+    "url": "/portfolio-3/static/media/carousel-img-6.72bf7ddd.jpg"
+  },
+  {
+    "revision": "9f5afb6a95a8e973eabd9c54f7b34398",
+    "url": "/portfolio-3/static/media/cat-1.9f5afb6a.jpg"
+  },
+  {
+    "revision": "e31d98c53b1ca50ce43b4c5567716c90",
+    "url": "/portfolio-3/static/media/cat-2.e31d98c5.jpg"
+  },
+  {
+    "revision": "60efe8da1a2022e4294026b85cd9e904",
+    "url": "/portfolio-3/static/media/cat-3.60efe8da.jpg"
+  },
+  {
+    "revision": "b8b9f48909e13cf5ed114c762192c5aa",
+    "url": "/portfolio-3/static/media/cat-4.b8b9f489.jpg"
+  },
+  {
+    "revision": "d07f0bcc54af4ac64ad04e19728db8b9",
+    "url": "/portfolio-3/static/media/cat-5.d07f0bcc.jpg"
+  },
+  {
+    "revision": "f3caa8a1c9fc19b9dda3bb6f6acac1bf",
+    "url": "/portfolio-3/static/media/cat-6.f3caa8a1.jpg"
+  },
+  {
+    "revision": "c7aef51c7e12ac4d6fb4ede6d6269c19",
+    "url": "/portfolio-3/static/media/cat.c7aef51c.jpg"
+  },
+  {
+    "revision": "6ef342554c12ff64c97155be6449f414",
+    "url": "/portfolio-3/static/media/food-img-1.6ef34255.jpg"
+  },
+  {
+    "revision": "f383148eb1c4ddb9c01d1e805515acb6",
+    "url": "/portfolio-3/static/media/food-img-2.f383148e.jpg"
+  },
+  {
+    "revision": "4f26d3114816207c67216d0e0512fc8b",
+    "url": "/portfolio-3/static/media/food-img-3.4f26d311.jpg"
+  },
+  {
+    "revision": "a2c102ac11cda4d146ddac76b1693fa8",
+    "url": "/portfolio-3/static/media/food-img-4.a2c102ac.jpg"
+  },
+  {
+    "revision": "a1c84c9800a51c8698c484c38107e3a9",
+    "url": "/portfolio-3/static/media/food-img-5.a1c84c98.jpg"
+  },
+  {
+    "revision": "ca00d448b90d67bbff1a4e4c258cd6a0",
+    "url": "/portfolio-3/static/media/food-img-6.ca00d448.jpg"
+  },
+  {
+    "revision": "34589e4dea5f42da0ab89fc5fb8c75b9",
+    "url": "/portfolio-3/static/media/food-img-7.34589e4d.jpg"
+  },
+  {
+    "revision": "27de5a48ac9c9aa02cd00f78dd98507d",
+    "url": "/portfolio-3/static/media/food-img-8.27de5a48.jpg"
+  },
+  {
+    "revision": "4fd4425022ebe9b7356bb00283dc4776",
+    "url": "/portfolio-3/static/media/res-img-1.4fd44250.jpg"
+  },
+  {
+    "revision": "9cae56bc37ddaef858ff078fa1d3f33d",
+    "url": "/portfolio-3/static/media/res-img-2.9cae56bc.jpg"
+  },
+  {
+    "revision": "855efec71db1869b74efcb61436ff970",
+    "url": "/portfolio-3/static/media/res-img-3.855efec7.jpg"
+  },
+  {
+    "revision": "af87f5242a99f04bd689a0f9690988af",
+    "url": "/portfolio-3/static/media/res-img-4.af87f524.jpg"
+  },
+  {
+    "revision": "f267f8e1ff44261436a6be4f9ce9419a",
+    "url": "/portfolio-3/static/media/res-img-5.f267f8e1.jpg"
+  },
+  {
+    "revision": "beeb7956d12a46878703d272ee5e1d2c",
+    "url": "/portfolio-3/static/media/res-img-6.beeb7956.jpg"
+  },
+  {
+    "revision": "b6a5658cd1442fa36e889e02235ba25b",
+    "url": "/portfolio-3/static/media/sushi.b6a5658c.jpg"
+  }
+]);
